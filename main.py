c2f56bedc3cf67a01e5396823467c15be93956ae
MOHAMMED ABDUL KHADER JEELANI <122251930+khaderjeelani@users.noreply.github.com>
2025-07-21 17:00:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, jsonify  # Import jsonify
 import numpy as np
 import pandas as pd
 import pickle
-
+import os
 
 # flask app
 app = Flask(__name__)
@@ -115,6 +115,8 @@ def blog():
     return render_template("blog.html")
 
 
-if __name__ == '__main__':
 
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
